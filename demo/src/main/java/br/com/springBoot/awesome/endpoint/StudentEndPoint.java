ac0414ee5a5820c86db9f76d707e4b8085565df1
luanoliveiradasilva <luanoliveira1107@gmail.com>
2020-06-20 17:39:38 -0300

    Método de requisição POST

--- demo/src/main/java/br/com/springBoot/awesome/endpoint/StudentEndPoint.java
+++ demo/src/main/java/br/com/springBoot/awesome/endpoint/StudentEndPoint.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,6 +41,12 @@ public class StudentEndPoint {
 
 		return new ResponseEntity<>(Student.studentList.get(index), HttpStatus.OK);
 
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<?> save(@RequestBody Student student) {
+		Student.studentList.add(student);
+		return new ResponseEntity<>(student, HttpStatus.OK);
 	}
 
 }
