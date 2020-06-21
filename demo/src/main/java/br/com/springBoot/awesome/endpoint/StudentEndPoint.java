4f8f734ad94910d7a6ed69aebe0cc7b9c664dc79
luanoliveiradasilva <luanoliveira1107@gmail.com>
2020-06-20 21:11:52 -0300

    Criado CRUD via spring com consulta no postman.

--- demo/src/main/java/br/com/springBoot/awesome/endpoint/StudentEndPoint.java
+++ demo/src/main/java/br/com/springBoot/awesome/endpoint/StudentEndPoint.java
@@ -3,10 +3,13 @@ package br.com.springBoot.awesome.endpoint;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.springBoot.awesome.error.CustomErrorType;
@@ -24,17 +27,20 @@ public class StudentEndPoint {
 		this.dateUtil = dateUtil;
 	}
 
-	@RequestMapping(method = RequestMethod.GET)
+//	@RequestMapping(method = RequestMethod.GET)
+	@GetMapping()
 	public ResponseEntity<?> listAll() {
 		return new ResponseEntity<>(Student.studentList, HttpStatus.OK);
 	}
 
 	/**
-	 * Método para fazer uma GET Resquest
+	 * Método GET Resquest
+	 * 
 	 * @param id
 	 * @return
 	 */
-	@RequestMapping(method = RequestMethod.GET, path = "/{id}")
+//	@RequestMapping(method = RequestMethod.GET, path = "/{id}")
+	@GetMapping(path = "/{id}")
 	public ResponseEntity<?> getStudentById(@PathVariable("id") int id) {
 		Student student = new Student();
 		student.setId(id);
@@ -49,15 +55,44 @@ public class StudentEndPoint {
 	}
 	
 	/**
-	 * Método para fazer um POST Request
+	 * Método POST Request
+	 * 
 	 * @param student
 	 * @return
 	 * 
 	 */
-	@RequestMapping(method = RequestMethod.POST)
+//	@RequestMapping(method = RequestMethod.POST)
+	@PostMapping
 	public ResponseEntity<?> save(@RequestBody Student student) {
 		Student.studentList.add(student);
 		return new ResponseEntity<>(student, HttpStatus.OK);
+	}
+
+	/**
+	 * Método DELETE Request
+	 * 
+	 * @param student
+	 * @return
+	 */
+//	@RequestMapping(method = RequestMethod.DELETE)
+	@DeleteMapping
+	public ResponseEntity<?> delete(@RequestBody Student student) {
+		Student.studentList.remove(student);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
+	/**
+	 * Método PUT Request
+	 * 
+	 * @param student
+	 * @return
+	 */
+//	@RequestMapping(method = RequestMethod.PUT)
+	@PutMapping
+	public ResponseEntity<?> update(@RequestBody Student student) {
+		Student.studentList.remove(student);
+		Student.studentList.add(student);
+		return new ResponseEntity<>(HttpStatus.OK);
 	}
 
 }
